bf910a814bf63d4bace8f87aefc3890caa6b71bf
xiang <xjn19920918@126.com>
2017-11-15 17:56:12 +0800

    搭建基本开发环境。

---
+++ src/main/java/com/competition/spring/CompetitionApplication.java
@@ -0,0 +1,14 @@
+package com.competition.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xiang
+ */
+@SpringBootApplication
+public class CompetitionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CompetitionApplication.class, args);
+    }
+}
